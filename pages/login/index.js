a07f73ed4ccb2c6d52e5cf0df75c380f39ba1c9e
Daler <dsarifkulo021@gmail.com>
2023-10-28 18:24:43 +0500

    fixed

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,28 +4,31 @@ let pass = document.querySelector('.password')
 let error = document.querySelector('.error')
 
 form.onsubmit = (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    let user = {}
+	let user = {}
 
-    let fm = new FormData(form)
+	let fm = new FormData(form)
 
-    fm.forEach((value, key) => {
-        user[key] = value
-        console.log(user);
-    })
+	fm.forEach((value, key) => {
+		user[key] = value
+	})
 
-    axios.get('http://localhost:8080/users?email=' + user.email)
-        .then((res) => {
-            if(res.status === 200 || res.status === 201) {
-                axios.get('http://localhost:8080/users?password=' + user.password)
-                    .then((res) => {
-                        if(res.data.password === pass.value) {
-                            location.assign('http://localhost:5173/')
-                            error.classList.add('hide')
-                        } 
-                    })
-                    }  
-                })
-            }
+	axios.get('http://localhost:8080/users?email=' + user.email)
+		.then((res) => {
+			if (res.status !== 200 && res.status !== 201) return
+			if(res.data.length === 0) {
+				alert('User not found')
+				return
+			}
 
+			let [res_user] = res.data
+
+			if(res_user.password === user.password) {
+				alert('welcome')
+				location.assign('/')
+			} else {
+				alert('wrong password')
+			}
+		})
+}
