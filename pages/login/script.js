23992cb3e8b53c6b693a651a02fbf6e468b52e39
Samir <sobiroff23@gmal.com>
2023-10-28 18:43:47 +0500

    added file

--- pages/login/script.js
+++ pages/login/script.js
@@ -1,4 +1,34 @@
+import axios from 'axios'
+let form = document.forms.login
 
 
+form.onsubmit = (e) => {
+    e.preventDefault();
+    
+    let user = {}
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        user[key] = value
+    })
 
 
+    axios.get('http://localhost:8080/users?email=' + user.email)
+        .then(res => {
+            if(res.status !== 200 && res.status !== 201) return 
+            if(res.data.length === 0) {
+                alert("not found")
+                return
+            } 
+
+            let [res_user] = res.data
+
+            if(res_user.password === user.password) {
+                alert('welcome')
+                location.assign('/')
+            } else {
+                alert('wrong')
+            }
+        })
+}
