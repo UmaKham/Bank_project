5f2580d21bce40417628194d2c651cf05aefe205
Samir <sobiroff23@gmal.com>
2023-10-28 17:00:56 +0500

    added files

--- pages/signUp/index.js
+++ pages/signUp/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 let form = document.forms.signup
 
+
 form.onsubmit = (e) => {
     e.preventDefault();
-    
+
     let user = {}
 
     let fm = new FormData(form)
@@ -15,20 +16,18 @@ form.onsubmit = (e) => {
 
     axios.get('http://localhost:8080/users?email=' + user.email)
         .then(res => {
-            if(res.status !== 200 && res.status !== 201) return 
-            if(res.data.length > 0) {
+            if (res.status !== 200 && res.status !== 201) return
+            if (res.data.length > 0) {
                 alert('account already taken!')
                 return
-            } 
+            }
 
             axios.post('http://localhost:8080/users', user)
                 .then(res => {
-                    if(res.status === 200 || res.status === 201)  {
+                    if (res.status === 200 || res.status === 201) {
                         location.assign('/pages/login/')
                     }
-
                 })
         })
-
-
 }
+
