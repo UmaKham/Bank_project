9fa1f043871383a9ffa845a3aa41b2b004fd96da
Daler <dsarifkulo021@gmail.com>
2023-10-31 18:24:00 +0500

    fixed

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,31 +1,33 @@
 import axios from 'axios'
-import {getData} from '../../modules/helpers'
+import {
+    getData
+} from '../../modules/helpers'
 
 let form = document.forms.add
 
 form.onsubmit = (e) => {
-	e.preventDefault();
+    e.preventDefault();
 
-	let user = {}
+    let user = {}
 
-	let fm = new FormData(form)
+    let fm = new FormData(form)
 
     fm.forEach((value, key) => {
         user[key] = value
     })
 
-    getData('/users?email=' + user.email) 
+    getData('/users?email=' + user.email)
         .then(res => {
-            if(!res) return
-            if(res.status !== 200 && res.status !== 201) return
-            if(res.data.length === 0) {
+            if (!res) return
+            if (res.status !== 200 && res.status !== 201) return
+            if (res.data.length === 0) {
                 alert('Not found')
                 return
-            } 
+            }
 
             let [res_user] = res.data
 
-            if(res_user.password === user.password) {
+            if (res_user.password === user.password) {
 
                 delete res_user.password
 
@@ -36,13 +38,4 @@ form.onsubmit = (e) => {
                 alert('Wrong password')
             }
         })
-            }
-
-			let [res_user] = res.data
-
-			if(res_user.password === user.password) {
-				alert('welcome')
-				location.assign('/')
-			} else {
-				alert('wrong password')
-			}
+}
