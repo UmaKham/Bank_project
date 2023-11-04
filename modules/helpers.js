05a91ffdae6434e54a224d1b2177e7d9dd20a9fc
MuniS67 <tursunovmunis34@gmail.com>
2023-11-04 10:43:34 +0500

    fixed

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,59 +1,55 @@
-import axios from "axios"
-const base_url = import.meta.env.VITE_BASE_URL
-
+import axios from "axios";
+const base_url = import.meta.env.VITE_BASE_URL;
 
 export const getData = async (resource) => {
-    try {
-        const res = await axios.get(base_url + resource)
-    
-        return res
-    } catch(e) {
-        console.log(e);
-        return null
-    }
-}
+  try {
+    const res = await axios.get(base_url + resource);
+
+    return res;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
 
 export const postData = async (resource, body) => {
-    try {
-        const res = await axios.post(base_url + resource, body)
+  try {
+    const res = await axios.post(base_url + resource, body);
 
-        return res
-    } catch(e) {
-        console.log(e);
-        return null
-    }
-    
-}
+    return res;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
 export const editData = async (resource, body) => {
-    try {
-        const res = await axios.patch(base_url + resource, body)
+  try {
+    const res = await axios.patch(base_url + resource, body);
 
-        return res
-    } catch(e) {
-        console.log(e);
-        return null
-    }
-    
-}
-
+    return res;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
 
 export const getSymbols = async () => {
-    const res = JSON.parse(localStorage.getItem('symbols'))
+  const res = JSON.parse(localStorage.getItem("symbols"));
 
-    if(res) {
-        return res
-    } 
+  if (res) {
+    return res;
+  }
 
-    try {
-        const res = await axios.get('https://api.apilayer.com/fixer/symbols', {
-            headers: {
-                apikey: import.meta.env.VITE_API_KEY
-            }
-        })
+  try {
+    const res = await axios.get("https://api.apilayer.com/fixer/symbols", {
+      headers: {
+        apikey: import.meta.env.VITE_API_KEY,
+      },
+    });
 
-        localStorage.setItem('symbols', JSON.stringify(res.data.symbols))
-        return res.data.symbols
-    } catch (e) {
-        console.log(e);
-    }
-}
+    localStorage.setItem("symbols", JSON.stringify(res.data.symbols));
+    return res.data.symbols;
+  } catch (e) {
+    console.log(e);
+  }
+};
