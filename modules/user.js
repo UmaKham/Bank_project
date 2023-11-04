a37d4eb984fa014b9711090038e78334ac11519b
Daler <dsarifkulo021@gmail.com>
2023-11-04 18:19:11 +0500

    fixed

--- modules/user.js
+++ modules/user.js
@@ -1,1 +1,9 @@
-export const user = JSON.parse(localStorage.getItem('user')) || null
+let user = localStorage.getItem('user')
+
+if(typeof(user) === 'undefined') {
+    localStorage.removeItem('user')
+}
+
+user = user ? JSON.parse(user) : null
+
+export {user}
