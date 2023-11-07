38c98b71f2fe9b6c7323b86239c139eed8049496
Daler <dsarifkulo021@gmail.com>
2023-11-07 19:11:17 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         my_wallets: resolve(__dirname, 'pages/my_wallets/index.html'),
         my_transactions: resolve(__dirname, 'pages/my_transactions/index.html'),
         addWallet: resolve(__dirname, 'pages/addWallet/index.html'),
-        addTrasnaction: resolve(__dirname, 'pages/addTrasnaction/index.html'),
+        addTransaction: resolve(__dirname, 'pages/addTransaction/index.html'),
         about_wallet: resolve(__dirname, 'pages/about_wallet/index.html')
       },
     },
